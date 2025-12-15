647249ed1edd70d2527d046dfc40fa077c5ce991
Lovys-prots66 <lovys6027@gmail.com>
2025-12-15 19:24:38 +0100

    Add delete controller

---
+++ controllers/userDeleteController.js
@@ -0,0 +1,25 @@
+import { responses } from "../config/responses";
+import remove from "../models/delete.js";
+import { sendResult } from "../utils/sendResult.js"
+import { sendError } from "../utils/sendError.js"
+
+class userDeleteController {
+    static async delete(req, res){
+        const { http, errors } = responses;
+
+        try {
+            const userId = req.params.id;
+
+            const [user] = await remove.removeUser(userId);
+
+            if(!user){
+                return sendError(res, http.NOT_FOUND, errors.USER_NOT_FOUND);
+            }
+
+            sendResult(res, http.SUCCESS, user);
+
+        } catch (error) {
+            sendError(res, http.SERVER_ERROR, errors.INTERNAL);
+        }
+    }
+}
